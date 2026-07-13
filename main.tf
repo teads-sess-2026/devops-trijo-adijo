7f3e0e6e55217dcd825e19ef9962c5606e37523b
nmiklavcic <nal.miklavcic@gmail.com>
2026-07-14 00:29:14 +0200

    Rewrite
    Made the subnet private a

--- main.tf
+++ main.tf
@@ -18,10 +18,27 @@ resource "aws_vpc" "main" { # named main for now change later if needed
 
 # -- AWS subnets -> 2 x public, 2 x private -- #
 
+# public subnets get .0 and .1 
+# private subnets get .10 and .11
+
 resource "aws_subnet" "public_a" {
+    vpc_id = aws_vpc.main.id # Reference that wires the subnet to the vpc
+    
+    cidr_block = "10.0.0.0/24" # public so it gets .0
+    
+    availability_zone = "eu-west-1a" # Which building it is on
+
+    tags = { Name = "trijo-adijo-public-a" }
+}
+
+resource "aws_subnet" "private_a" {
     vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.0.0/24"
+
+    cidr_block = "10.0.10.0/24" # private so it gets .10
+
     availability_zone = "eu-west-1a"
+
+    tags = { Name = "trijo-adijo-private-a" }
 }
 
 # -- -- #
