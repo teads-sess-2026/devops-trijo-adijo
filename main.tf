44b75be998afe5e06d6982116320e03332671228
nmiklavcic <nal.miklavcic@gmail.com>
2026-07-14 00:31:53 +0200

    Rewrite
    Made the subnets for the second AZ.

--- main.tf
+++ main.tf
@@ -41,4 +41,24 @@ resource "aws_subnet" "private_a" {
     tags = { Name = "trijo-adijo-private-a" }
 }
 
+resource "aws_subnet" "public_b" {
+    vpc_id = aws_vpc.main.id
+
+    cidr_block = "10.0.1.0/24" 
+
+    availability_zone = "eu-west-1b"
+
+    tags = { Name = "trijo-adijo-public-b" }
+}
+
+resource "aws_subnet" "private_b" {
+    vpc_id = aws_vpc.main.id
+
+    cidr_block = "10.0.11.0/24" 
+
+    availability_zone = "eu-west-1b"
+
+    tags = { Name = "trijo-adijo-private-b" }
+}
+
 # -- -- #
