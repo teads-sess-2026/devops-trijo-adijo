5563aafecb74710d67265735a5819a6ac47eedeb
nmiklavcic <nal.miklavcic@gmail.com>
2026-07-14 00:06:15 +0200

    Rewrite
    Fixed typo.

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # This file holds the actual infra
 
 resource "aws_vpc" "main" { # tagged with main for now change later if needed
-    cidr-block = "10.0.0.0/16"
+    cidr_block = "10.0.0.0/16"
 
     # The following are there to make the VPC's internal DNS work
     # Without theese instances don't get internal DNS names
