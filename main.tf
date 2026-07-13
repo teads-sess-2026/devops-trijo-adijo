eb2c11bc4763523802439b87521930a500f997db
nmiklavcic <nal.miklavcic@gmail.com>
2026-07-14 00:04:04 +0200

    Rewrite
    Made main.tf the file that will hold our resources and rewrote it to a basic vpc.

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
-provider "aws" {
-  region  = "eu-west-1"
-  profile = "summer-school"
+# This file holds the actual infra
+
+resource "aws_vpc" "main" { # tagged with main for now change later if needed
+    cidr-block = "10.0.0.0/16"
+
+    # The following are there to make the VPC's internal DNS work
+    # Without theese instances don't get internal DNS names
+    # Makes EKS sad !!
+    enable_dns_support = true # defaults to true
+    enable_dns_hostnames = true # defaults to false 
+
+    tags = { Name = "trijo-adijo"}
 }
