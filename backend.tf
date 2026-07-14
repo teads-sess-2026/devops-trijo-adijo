3290e2de0183bde9b98918b25e92aa9e9996092f
nmiklavcic <nal.miklavcic@gmail.com>
2026-07-14 11:27:29 +0200

    Move backend to terraform.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "trijo-adijo-tfstate"
-    key          = "vpc-eks/terraform.tfstate"
-    region       = "eu-west-1"
-    encrypt      = true
-    use_lockfile = true   # native S3 locking (Terraform 1.11+, DynamoDB ni potreben)
-  }
-}
--- terraform.tf
+++ terraform.tf
@@ -12,6 +12,13 @@ terraform {
 			version = "~> 6.0"	# Allow versions 6 -> 7
 		}
 	}
+	backend "s3" {
+		bucket       = "trijo-adijo-tfstate"
+		key          = "vpc-eks/terraform.tfstate"
+		region       = "eu-west-1"
+		encrypt      = true
+		use_lockfile = true   # native S3 locking (Terraform 1.11+, DynamoDB ni potreben)
+  	}
 	required_version = ">= 1.15" # Minimum required version is 1.15
 }
 
